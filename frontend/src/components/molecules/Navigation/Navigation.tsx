84e636aa102a85e417f9e845dcd451285452639b
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-10 16:38:54 +0100

    Create and use <Logo/>

--- frontend/src/components/molecules/Navigation/Navigation.tsx
+++ frontend/src/components/molecules/Navigation/Navigation.tsx
@@ -1,22 +1,29 @@
+import Logo from 'components/atoms/Logo/Logo'
+
 export default function Navigation() {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="border-b border-gray-200">
         <div className="items-cente flex h-16 justify-between">
           {/* ====================== Logo (lg+) ====================== */}
-          <div className="hidden lg:flex lg:items-center">Logo</div>
+          <div className="hidden lg:flex lg:items-center">
+            <Logo />
+          </div>
 
           {/* ====================== Desktop Menu (lg+) ====================== */}
           <div className="hidden h-full lg:flex lg:items-center">
             Menu desktop
           </div>
-          {/* ====================== Mobile Menu and Search (lg-) ====================== */}
+
+          {/* ====================== Hamburger and Search (lg-) ====================== */}
           <div className="flex flex-1 items-center lg:hidden">
-            Mobile Menu and Search
+            Hamburger and Search
           </div>
 
           {/* ====================== Logo (lg-) ====================== */}
-          <div className="flex items-center lg:hidden">Logo</div>
+          <div className="flex items-center lg:hidden">
+            <Logo />
+          </div>
 
           {/* ====================== Rest (lg+ and lg-) ====================== */}
           <div className="flex flex-1 items-center justify-end">Rest</div>
