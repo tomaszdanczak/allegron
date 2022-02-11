249cfb0706d9f815f6184c4198ef0731b8c1a317
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-11 15:27:22 +0100

    Add links in the <Banner/>

--- frontend/src/components/atoms/Banner/Banner.tsx
+++ frontend/src/components/atoms/Banner/Banner.tsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom'
+
 export default function Banner() {
   return (
-    <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-      Get free delivery on orders over $100
-    </p>
+    <div className="bg-indigo-600">
+      <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+        <span className="hidden lg:block lg:flex-1"></span>
+
+        <p className="flex-1 text-center text-sm font-medium text-white lg:flex-none">
+          Get free delivery on orders over $100
+        </p>
+
+        <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+          <Link
+            to="/register"
+            className="text-sm font-medium text-white hover:text-gray-100"
+          >
+            Create an account
+          </Link>
+
+          <span className="h-6 w-px bg-gray-600" aria-hidden="true" />
+
+          <Link
+            to="/signin"
+            className="text-sm font-medium text-white hover:text-gray-100"
+          >
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
