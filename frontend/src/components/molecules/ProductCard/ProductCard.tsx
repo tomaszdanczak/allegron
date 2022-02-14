269efd3f264693bf0155534d3352ef2d3b1623b4
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-14 17:29:09 +0100

    Create and use <ProductCard/>

---
+++ frontend/src/components/molecules/ProductCard/ProductCard.tsx
@@ -0,0 +1,35 @@
+import { IProduct } from 'types/product'
+import { useNavigate } from 'react-router-dom'
+
+export default function ProductCard({ product }: { product: IProduct }) {
+  const { color, images, name, prices, _id } = product
+
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/products/${_id}`)
+  }
+  return (
+    <div className="group relative" onClick={handleClick}>
+      <div className="min-h-80 aspect-w-1 aspect-h-1 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+        <img
+          src={images[0].imageSrc}
+          alt={images[0].imageAlt}
+          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <p>
+              <span aria-hidden="true" className="absolute inset-0" />
+              {name}
+            </p>
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{color}</p>
+        </div>
+        <p className="text-sm font-medium text-gray-900">{prices[0].price}</p>
+      </div>
+    </div>
+  )
+}
--- frontend/src/views/HomeScreen/HomeScreen.tsx
+++ frontend/src/views/HomeScreen/HomeScreen.tsx
@@ -1,17 +1,21 @@
 import { useGetProductsQuery } from 'app/api/productsApi'
 import LoadingBox from 'components/atoms/LoadingBox/LoadingBox'
 import MessageBox from 'components/atoms/MessageBox/MessageBox'
+import ProductCard from 'components/molecules/ProductCard/ProductCard'
 
 let errorMsg: string
 
 export default function HomeScreen() {
-  const { data, isLoading, isError, error = {} } = useGetProductsQuery()
+  const {
+    data: products = [],
+    isLoading,
+    isError,
+    error = {},
+  } = useGetProductsQuery()
 
   if ('originalStatus' in error) {
     errorMsg = `Request failed with status code ${error.originalStatus}`
   }
-
-  console.log('data:', data)
 
   return (
     <>
@@ -24,7 +28,9 @@ export default function HomeScreen() {
         <MessageBox variant="error">{errorMsg}</MessageBox>
       ) : (
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          products
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
         </div>
       )}
     </>
