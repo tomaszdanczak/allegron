86d7c72dd147c56dddaa5645c9763c3ca3b6c39a
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-14 14:33:54 +0100

    Create and use <LoadingBox/>

--- frontend/src/views/HomeScreen/HomeScreen.tsx
+++ frontend/src/views/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,23 @@
 import { useGetProductsQuery } from 'app/api/productsApi'
+import LoadingBox from 'components/atoms/LoadingBox/LoadingBox'
 
 export default function HomeScreen() {
-  const { data } = useGetProductsQuery()
+  const { data, isLoading } = useGetProductsQuery()
 
   console.log('data:', data)
 
-  return <div>HomeScreen</div>
+  return (
+    <>
+      <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+        Customers also purchased
+      </h2>
+      {isLoading ? (
+        <LoadingBox />
+      ) : (
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          products
+        </div>
+      )}
+    </>
+  )
 }
