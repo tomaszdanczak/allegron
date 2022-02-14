57c90695123b71669dfff8674d7cb12a6942c615
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-14 09:43:16 +0100

    Add simple routing and <CartScreen/>

--- frontend/src/views/Root/Root.tsx
+++ frontend/src/views/Root/Root.tsx
@@ -1,10 +1,15 @@
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate'
+import { Routes, Route } from 'react-router-dom'
+import CartScreen from 'views/CartScreen/CartScreen'
 import HomeScreen from 'views/HomeScreen/HomeScreen'
 
 function Root() {
   return (
     <MainTemplate>
-      <HomeScreen />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
+      </Routes>
     </MainTemplate>
   )
 }
