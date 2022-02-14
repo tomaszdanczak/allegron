8f045c28c7c8afe1f280e037d6e1b287d4cba5c9
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-14 10:20:35 +0100

    Create the simplest server

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello express");
+});
+
+app.listen(5000, () => {
+  console.log(`Serve at http://localhost:5000`);
+});
