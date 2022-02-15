fb0b5d723eb8de1afe41cf21035d35b2968e82c8
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-15 09:26:04 +0100

    Display loading state

--- frontend/src/views/ProductScreen/ProductScreen.tsx
+++ frontend/src/views/ProductScreen/ProductScreen.tsx
@@ -1,16 +1,21 @@
 import { useGetProductQuery } from 'app/api/productApi'
+import LoadingBox from 'components/atoms/LoadingBox/LoadingBox'
 import { useParams } from 'react-router-dom'
 import { IProduct } from 'types/product'
 
 export default function ProductScreen() {
   const params = useParams()
   const { id: productId = '' } = params
-  const { data: product = {} as IProduct } = useGetProductQuery(productId)
-  console.log('data:', product)
+  const { data: product = {} as IProduct, isLoading } =
+    useGetProductQuery(productId)
 
   return (
     <div>
-      <pre>{JSON.stringify(product, null, 2)}</pre>
+      {isLoading ? (
+        <LoadingBox />
+      ) : (
+        <pre>{JSON.stringify(product, null, 2)}</pre>
+      )}
     </div>
   )
 }
