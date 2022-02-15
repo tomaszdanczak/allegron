3780fa17680661102cf3ce5a21ca9ad0a223ed08
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-15 09:43:44 +0100

    Display error state

--- frontend/src/views/ProductScreen/ProductScreen.tsx
+++ frontend/src/views/ProductScreen/ProductScreen.tsx
@@ -1,18 +1,32 @@
 import { useGetProductQuery } from 'app/api/productApi'
 import LoadingBox from 'components/atoms/LoadingBox/LoadingBox'
+import MessageBox from 'components/atoms/MessageBox/MessageBox'
 import { useParams } from 'react-router-dom'
 import { IProduct } from 'types/product'
+
+let errorMsg: string
 
 export default function ProductScreen() {
   const params = useParams()
   const { id: productId = '' } = params
-  const { data: product = {} as IProduct, isLoading } =
-    useGetProductQuery(productId)
+  const {
+    data: product = {} as IProduct,
+    isLoading,
+    isError,
+    error = {},
+  } = useGetProductQuery(productId)
+
+  // TODO: Type guard
+  if ('originalStatus' in error) {
+    errorMsg = `Request failed with status code ${error.originalStatus}`
+  }
 
   return (
     <div>
       {isLoading ? (
         <LoadingBox />
+      ) : isError ? (
+        <MessageBox variant="error">{errorMsg}</MessageBox>
       ) : (
         <pre>{JSON.stringify(product, null, 2)}</pre>
       )}
