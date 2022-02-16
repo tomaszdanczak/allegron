84d955e0afcfbabb5e3ea5da44d026defb69f7e4
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-16 09:33:32 +0100

    Create and use <CartItemImage/>

---
+++ frontend/src/components/atoms/CartItemImage/CartItemImage.tsx
@@ -0,0 +1,17 @@
+import { IImage } from 'types/product'
+
+interface IProps {
+  image: IImage
+}
+
+export default function CartItemImage({ image }: IProps) {
+  return (
+    <div className="flex-shrink-0">
+      <img
+        src={image.imageSrc}
+        alt={image.imageAlt}
+        className="h-24 w-24 rounded-md object-cover object-center sm:h-48 sm:w-48"
+      />
+    </div>
+  )
+}
