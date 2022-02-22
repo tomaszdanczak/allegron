c6f539e35dec9e891b3314ea77e980dac2fd39d2
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-22 10:26:42 +0100

    create template of <RegisterScreen/> and <RegisterScreenHeader/>

--- frontend/src/views/RegisterScreen/RegisterScreen.tsx
+++ frontend/src/views/RegisterScreen/RegisterScreen.tsx
@@ -1,3 +1,15 @@
+import RegisterScreenHeader from 'components/molecules/RegisterScreenHeader/RegisterScreenHeader'
+
 export default function RegisterScreen() {
-  return <div>RegisterScreen</div>
+  return (
+    <div className="h-full bg-gray-50 py-12">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <RegisterScreenHeader />
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">RegisterForm</div>
+      </div>
+    </div>
+  )
 }
