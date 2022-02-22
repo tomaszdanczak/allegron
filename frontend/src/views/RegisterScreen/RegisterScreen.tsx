55526768d0cefc0406e75228b6b99434308688eb
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-22 12:04:13 +0100

    Redirect user to <HomeScreen/> when user is correctly registered

--- frontend/src/views/RegisterScreen/RegisterScreen.tsx
+++ frontend/src/views/RegisterScreen/RegisterScreen.tsx
@@ -1,7 +1,21 @@
+import { selectCurrentUser } from 'app/authSlice'
 import RegisterScreenHeader from 'components/molecules/RegisterScreenHeader/RegisterScreenHeader'
 import RegisterForm from 'components/organisms/RegisterForm/RegisterForm'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 export default function RegisterScreen() {
+  const userInfo = useSelector(selectCurrentUser)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (userInfo?.name) {
+      navigate('/')
+    }
+  }, [userInfo, navigate])
+
   return (
     <div className="h-full bg-gray-50 py-12">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
