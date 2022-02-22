a405f428356fafbebc8b6a145f6434810c7a92ad
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-22 13:29:18 +0100

    Redirect to <SigninScreen/> when user is logged out

--- frontend/src/views/ShippingAddressScreen/ShippingAddressScreen.tsx
+++ frontend/src/views/ShippingAddressScreen/ShippingAddressScreen.tsx
@@ -1,3 +1,17 @@
+import { selectCurrentUser } from 'app/authSlice'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 export default function ShippingAddressScreen() {
+  const userInfo = useSelector(selectCurrentUser)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!userInfo.name) {
+      navigate('/signin')
+    }
+  }, [userInfo, navigate])
+
   return <div>ShippingAddressScreen</div>
 }
