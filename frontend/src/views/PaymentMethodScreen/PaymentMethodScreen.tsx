d742a82e31a7c126752fbe6a89881771b63f52c5
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-23 14:07:26 +0100

    Redirect user from <PaymentMethodScreen/> to <ShippingAddressScreen/> if shippingInfo.address is empty string

--- frontend/src/views/PaymentMethodScreen/PaymentMethodScreen.tsx
+++ frontend/src/views/PaymentMethodScreen/PaymentMethodScreen.tsx
@@ -1,7 +1,19 @@
 import CheckoutSteps from 'components/molecules/CheckoutSteps/CheckoutSteps'
 import PaymentMethodForm from 'components/organisms/PaymentMethodForm/PaymentMethodForm'
+import { useEffect } from 'react'
+import { useShippingInfo } from 'hooks/useShippingInfo'
+import { useNavigate } from 'react-router-dom'
 
 export default function PaymentMethodScreen() {
+  const { shippingInfo } = useShippingInfo()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (shippingInfo.address === '') {
+      navigate('/shipping')
+    }
+  }, [shippingInfo, navigate])
+
   return (
     <div>
       <CheckoutSteps currentStep={3} />
