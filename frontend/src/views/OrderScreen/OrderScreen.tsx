50301067dcc4c87569851dee259ced0eb90a5c27
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-24 16:20:44 +0100

    Create and use simple <OrderScreen/>

---
+++ frontend/src/views/OrderScreen/OrderScreen.tsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom'
+
+export default function OrderScreen() {
+  const params = useParams()
+  const { id: orderId = '' } = params
+  return (
+    <div>
+      OrderScreen
+      <div>{orderId}</div>
+    </div>
+  )
+}
