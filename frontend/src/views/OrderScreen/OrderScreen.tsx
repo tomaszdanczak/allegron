d0c295d928a3d8010136e9a6f1fff9e380913d93
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2022-02-25 12:24:51 +0100

    Create and use useGetOrderQuery

--- frontend/src/views/OrderScreen/OrderScreen.tsx
+++ frontend/src/views/OrderScreen/OrderScreen.tsx
@@ -1,8 +1,13 @@
+import { useGetOrderQuery } from 'app/api/ordersApi'
 import { useParams } from 'react-router-dom'
 
 export default function OrderScreen() {
   const params = useParams()
   const { id: orderId = '' } = params
+
+  const response = useGetOrderQuery(orderId)
+
+  console.log('response:', response)
 
   return (
     <div>
